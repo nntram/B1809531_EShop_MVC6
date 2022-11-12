dccaec2825ec970393c46e8af187e9714b85819a
nntram <tramb9531@gmail.com>
2022-11-12 10:55:45 +0700

    Add project files.

---
+++ B1809531_EShop_MVC6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B1809531_EShop_MVC6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
